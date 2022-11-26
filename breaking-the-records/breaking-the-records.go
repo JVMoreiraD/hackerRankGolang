df9288e0a8952fd568ba87137ed01460c69487fa
jvmoreirad <jvitormoreirad@hotmail.com>
2022-11-25 23:18:14 -0300

    feat: breaking the records problem

---
+++ breaking-the-records/breaking-the-records.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+func breakingRecords(scores []int32) []int32 {
+	// Write your code here
+	var records = make([]int32, 2)
+	var max int32
+	var lowest int32
+	for i, v := range scores {
+		switch i {
+		case 0:
+			max, lowest = v, v
+		default:
+			if v > max {
+				records[0]++
+				max = v
+			}
+			if v < lowest {
+				records[1]++
+				lowest = v
+			}
+
+		}
+	}
+	return records
+
+}
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	scoresTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var scores []int32
+
+	for i := 0; i < int(n); i++ {
+		scoresItemTemp, err := strconv.ParseInt(scoresTemp[i], 10, 64)
+		checkError(err)
+		scoresItem := int32(scoresItemTemp)
+		scores = append(scores, scoresItem)
+	}
+
+	result := breakingRecords(scores)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
